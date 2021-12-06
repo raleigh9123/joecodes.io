62d2e765cd4e8acbd12d09533dc843cc15329cbc
Joseph Butterfield <raleigh9123@gmail.com>
2021-12-06 02:06:53 -0800

    update next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+
+const { withPlaiceholder } = require("@plaiceholder/next");
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+};
+
+module.exports = withPlaiceholder(nextConfig);
